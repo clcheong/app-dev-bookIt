30cff24896fdcd138aee7dd3266cbe997873084c
clcheong <clcheong00@gmail.com>
2021-11-10 01:09:14 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,306 @@
+# My own Library
+# from course import get_course_all, get_course_no_repeat, get_course_section, get_course_detail
+
+# Library from Flask
+from bigquery import getTotalRecycledKG,getTotalTreesPlanted,getTotalEnergySaved,getBranchCount,getCurrentMonth,getTotalCFcurrMonth,getCulmulativeCFcurrMonth,getCurrentYear,getStaffCount,getCurrentDay
+from flask import Flask, render_template, request, redirect, session, flash, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+# from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+
+"""
+==================================================================================================
+||        Data Base Setup                                                                       ||
+==================================================================================================
+"""
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://mzqzzqbbqshuyu:8dac9f5ddbfbc9f7556554c9d4d7b101acc45c7b2d8bce1c9ae90931f464c22f@ec2-54-205-61-191.compute-1.amazonaws.com:5432/d55vjv88et7bmh"
+# app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+# db = SQLAlchemy(app)
+
+# class users(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     studentID = db.Column(db.String(10), nullable=False, unique=True)
+#     phash = db.Column(db.String(120), nullable=False)
+
+# class registers(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     studentID = db.Column(db.String(10), nullable=False)
+#     courseID = db.Column(db.String(10), nullable=False)
+#     section = db.Column(db.Integer, nullable=False)
+
+
+"""
+==================================================================================================
+||        App Route                                                                             ||
+==================================================================================================
+"""
+@app.route('/')
+
+@app.route('/index')
+def index():
+    # branchCount = getBranchCount()
+    # staffCount = getStaffCount()
+    # day = getCurrentDay()
+    # month = getCurrentMonth()
+    # year = getCurrentYear()
+    # currTotalCF = round(getTotalCFcurrMonth() * 0.001,3)
+    # avgCurrTotalCF = round(currTotalCF/branchCount,3)
+    # culTotalCFcurrMonth = round(getCulmulativeCFcurrMonth()*0.001,3)
+    # avgCulTotal = round(culTotalCFcurrMonth/branchCount,3)
+    # currMonthCFperCapita = round(currTotalCF/staffCount,3)
+    # safePerCapita = round(4/12,3)
+    # totSafe = round(staffCount*safePerCapita/branchCount,3)
+    
+    # kgRecycled = round(getTotalRecycledKG(),3)
+    # treesPlanted = round(getTotalTreesPlanted(),3)
+    # energySaved = round(getTotalEnergySaved(),3)
+    
+    return render_template('index.html')#,energySaved=energySaved,treesPlanted=treesPlanted,kgRecycled=kgRecycled,branchCountHTML=branchCount,totSafe=totSafe,staffCount=staffCount,currMonth=month,currYear=year, totalCF=currTotalCF, avgCF=avgCurrTotalCF, culTotal=culTotalCFcurrMonth, avgCulTotal=avgCulTotal,currMonthCFperCapita=currMonthCFperCapita,safe=safePerCapita,currDay=day)
+
+
+@app.route('/login')
+def login():
+    return render_template('pages-login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('pages-register.html')
+
+# @app.route('/forgot-password')
+# def forgotpassword():
+#     return render_template('forgot-password.html')
+
+
+@app.route('/charts-apexcharts')
+def apexcharts():
+    # month = getCurrentMonth()
+    # year = getCurrentYear()
+    return render_template('charts-apexcharts.html')#,currMonth = month,currYear=year)
+
+@app.route('/charts-chartjs')
+def chartsjs():
+    return render_template('charts-chartjs.html')
+
+@app.route('/charts-echarts')
+def echarts():
+    return render_template('charts-echarts.html')
+
+@app.route('/tables-data')
+def tablesdata():
+    return render_template('tables-data.html')
+
+
+@app.route('/tables-general')
+def tablesgeneral():
+    return render_template('tables-general.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('pages-contact.html')
+
+@app.route('/404')
+def four0four():
+    return render_template('pages-error-404.html')
+
+
+@app.route('/blank')
+def blank():
+    return render_template('pages-blank.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('users-profile.html')
+
+@app.route('/faq')
+def faq():
+    return render_template('pages-faq.html')
+
+# @app.route('/buttons')
+# def buttons():
+#     return render_template('buttons.html')
+
+# @app.route('/cards')
+# def cards():
+#     return render_template('cards.html')
+
+# @app.route('/layout')
+# def layout():
+#     return render_template('layout.html')
+
+
+# @app.route('/utilities-animation')
+# def utilitiesAnimation():
+#     return render_template('utilities-animation.html')
+
+# @app.route('/utilities-border')
+# def utilitiesBorder():
+#     return render_template('utilities-border.html')
+
+# @app.route('/utilities-color')
+# def utilitiesColor():
+#     return render_template('utilities-color.html')
+
+# @app.route('/utilities-other')
+# def utilitiesOther():
+#     return render_template('utilities-other.html')
+
+# @app.route('/update-consumption',methods=['GET','POST']) 
+# def updateConsumption():
+#     day = getCurrentDay()
+#     month = getCurrentMonth()
+#     year = getCurrentYear()
+    
+#     if request.method == 'POST':
+#         form = request.form.get(exampleFirstName)
+#         flask(f"update {form}")
+#         return redirect(url_for("index"))
+    
+#     return render_template('update-consumption.html',currMonth=month,currYear=year)
+
+
+# @app.route('/home')
+# def home():
+#     studentID = None
+#     course_registered = []
+#     if 'user' in session:
+#         studentID = session['user']
+
+#         user_course = registers.query.filter_by(studentID=studentID).all()
+#         for course in user_course:
+#             course_registered.append(get_course_detail(course.courseID, str(course.section)))
+
+#     return render_template('home.html', studentID=studentID, courses=course_registered)
+
+# @app.route('/course')
+# def course():
+#     list_courses = get_course_no_repeat()
+#     return render_template('course.html', courses=list_courses)
+
+# @app.route('/section')
+# def section():
+#     # Get Method
+#     courseID = request.args.get('courseID')
+#     section_list = get_course_section(courseID)
+    
+#     return render_template('section.html', sections=section_list, courseID=courseID)
+
+# @app.route('/enrollment')
+# def enrollment():
+#     courseID = request.args.get('courseID')
+#     section = request.args.get('section')
+
+#     if not 'user' in session:
+#         flash("Please Login to Enrol in the Course!")
+#         return redirect(url_for('login'))
+
+#     studentID = session['user']
+#     course_registered = registers.query.filter_by(studentID=studentID).filter_by(courseID=courseID).first()
+
+#     if course_registered:
+#         flash(f"Error! {courseID} already registered!",)
+#         return redirect(url_for('course'))
+
+#     course = registers(studentID=studentID, courseID=courseID, section=section)
+#     db.session.add(course)
+#     db.session.commit()
+
+#     flash(f"{courseID} register successfully.", "register")
+#     return redirect(url_for('home'))
+
+# @app.route('/remove', methods=['GET', 'POST'])
+# def remove():
+#     studentID = None
+#     if 'user' in session:
+#         studentID = session['user']
+
+#     if request.method == "GET":
+#         user_courses = []
+#         db_user_courses = registers.query.filter_by(studentID=studentID).all()
+#         for course in db_user_courses:
+#             user_courses.append(course.courseID)
+    
+#         return render_template('remove.html', courses=user_courses)
+    
+#     else:
+#         courseID = request.form.get('course')
+         
+#         course = registers.query.filter_by(studentID=studentID).filter_by(courseID=courseID).first()
+#         db.session.delete(course)
+#         db.session.commit()
+
+#         flash(f"{courseID} removed.", "remove")
+#         return redirect(url_for('home'))
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == "GET":
+#         return render_template('login.html')
+
+#     else:
+#         studentID = request.form.get('inputStudentID')
+#         password = request.form.get('inputPassword')
+
+#         user_result = users.query.filter_by(studentID=studentID).first()
+
+#         if not user_result:
+#             flash('StudentID or Password incorrect!')
+#             return redirect(url_for('login'))
+
+#         elif check_password_hash(user_result.phash, password):
+#             session['user'] = studentID
+#             return redirect(url_for('home'))
+
+#         else:
+#             flash('StudentID or Password incorrect!')
+#             return redirect(url_for('login'))
+
+# @app.route('/register', methods=['GET', 'POST'])
+# def register():
+#     if request.method == "GET":
+#         return render_template('register.html')
+
+#     else:
+#         studentID = request.form.get('inputStudentID')
+#         password = request.form.get('inputPassword')
+#         retype = request.form.get('inputRetype')
+#         phash = None
+
+#         if password == retype:
+#             phash = generate_password_hash(password)
+
+#             # Check student used or not
+#             user_result = users.query.filter_by(studentID=studentID).first()
+
+#             if user_result:
+#                 flash("Student ID have been register! Please SignIn")
+#                 return redirect(url_for('register'))
+
+#         elif len(studentID) > 9:
+#             flash("Student ID Invalid! Please try again.")
+#             return redirect(url_for('register'))
+
+#         else:
+#             flash("Password not same! Please try again.")
+#             return redirect(url_for('register'))
+
+#         # Create new user in database
+#         user = users(studentID = studentID, phash = phash)
+#         db.session.add(user)
+#         db.session.commit()
+        
+#         # Update user in session
+#         session['user'] = studentID
+
+#         flash("Account Created", "register")
+#         return redirect(url_for('home'))
+
+# @app.route('/logout')
+# def logout():
+#     session.pop('user', None)
+#     return redirect(url_for('home'))
---
+++ bigquery.py
@@ -0,0 +1,147 @@
+
+from google.cloud import bigquery
+from datetime import datetime
+#set GOOGLE_APPLICATION_CREDENTIALS=C:\Users\User\Downloads\hlb-carbon-footprint-ps6\turing-goods-324908-d52fcabab09f.json
+
+def getAvgCFB001():
+    # Construct a BigQuery client object.
+    client = bigquery.Client()
+
+    query = """
+        SELECT AVG(totalCarbonFootprint) AS avg_CF
+        FROM `hlbcyhi2021-2.main.b001-performance-updated`
+    """
+    query_job = client.query(query)  # Make an API request.
+
+    print("The query data:")
+    # print(query_job[0]["avg_CF"])
+    for row in query_job:
+        # print(type(query_job))
+        # Row values can be accessed by field name or index.
+        # print("name={}, count={}".format(row[0], row["total_people"]))
+        return(row["avg_CF"])
+        
+    # return query_job[0]["avg_CF"]
+    
+    
+def getBranchCount():
+    client = bigquery.Client()
+    
+    query = """
+        SELECT COUNT(BranchID) as branchCount
+        FROM `hlbcyhi2021-2.main.main-table`
+        WHERE EXTRACT(Month FROM Date) = 1
+    """
+    query_job = client.query(query)
+    
+    # print("The query data:")
+    for row in query_job:
+        return(row["branchCount"])
+    
+
+
+def getCurrentDay():
+    day = datetime.now().strftime("%d")
+    return day
+
+
+def getCurrentMonth():
+    month = datetime.now().strftime('%B')
+    return month
+
+def getCurrentYear():
+    year = datetime.now().strftime('%Y')
+    return year
+
+def getStaffCount():
+    client = bigquery.Client()
+    
+    query="""
+        SELECT SUM(staffCount) as staffCount
+        FROM `hlbcyhi2021-2.main.main-table`
+        WHERE EXTRACT(Month FROM Date) = EXTRACT(Month FROM CURRENT_DATE())
+    """    
+    query_job = client.query(query)
+    # print("The query data:")
+    for row in query_job:
+        return(row["staffCount"])    
+    
+    
+def getTotalCFcurrMonth():
+    client = bigquery.Client()
+    
+    query="""
+        SELECT SUM(totalCF_kgCO2) AS totalCF
+        FROM `hlbcyhi2021-2.main.main-table`
+        WHERE EXTRACT(Month FROM Date) = EXTRACT(Month FROM CURRENT_DATE())
+    """
+    query_job = client.query(query)
+    # print("The query data:")
+    for row in query_job:
+        return(row["totalCF"])
+    
+    
+def getCulmulativeCFcurrMonth():
+    client = bigquery.Client()
+    
+    query="""
+        SELECT SUM(totalCF_kgCO2) as culTotal
+        FROM `hlbcyhi2021-2.main.main-table`
+        WHERE EXTRACT(Month FROM Date) <= EXTRACT(Month FROM CURRENT_DATE())
+    """
+    
+    query_job = client.query(query)
+    # print("The query data:")
+    for row in query_job:
+        return(row["culTotal"])
+    
+    
+def getTotalRecycledKG():
+    client = bigquery.Client()
+    
+    query="""
+        SELECT SUM(totalKG) AS totalKGrecycled
+        FROM `hlbcyhi2021-2.main.hlb-recycle`
+        WHERE EXTRACT(Month FROM Date) = EXTRACT(Month FROM CURRENT_DATE())
+    """
+    
+    query_job = client.query(query)
+    # print("The query data:")
+    for row in query_job:
+        return(row["totalKGrecycled"])    
+    
+    
+    
+def getTotalTreesPlanted():
+    client = bigquery.Client()
+    
+    query="""
+        SELECT SUM(treesPlanted) AS totalTreesPlanted
+        FROM `hlbcyhi2021-2.main.hlb-recycle`
+        WHERE EXTRACT(Month FROM Date) = EXTRACT(Month FROM CURRENT_DATE())
+    """
+    
+    query_job = client.query(query)
+    # print("The query data:")
+    for row in query_job:
+        return(row["totalTreesPlanted"])  
+    
+    
+    
+def getTotalEnergySaved():
+    client = bigquery.Client()
+    
+    query="""
+        SELECT SUM(energySavedkwh) AS totalEnergySaved
+        FROM `hlbcyhi2021-2.main.hlb-recycle`
+        WHERE EXTRACT(Month FROM Date) = EXTRACT(Month FROM CURRENT_DATE())
+    """
+    
+    query_job = client.query(query)
+    # print("The query data:")
+    for row in query_job:
+        return(row["totalEnergySaved"])  
+
+    
+if __name__ == "__main__":
+    print(getTotalEnergySaved())
