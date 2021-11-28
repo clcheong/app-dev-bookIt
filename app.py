193ef64f018a26f2193944708bd4e84b8929d6a9
Brandon <brandon_1331@hotmail.com>
2021-11-28 10:13:06 +0800

    Jsonify

--- app.py
+++ app.py
@@ -209,96 +209,74 @@ def IndexResident():
         SELECT EXTRACT(HOUR FROM Start_Time) as hour,Start_Time,Booking
         FROM main.Court1
         """
-        stime1=""
-        stime2=""
-        stime3=""
-        stime4=""
-        stime5=""
-        stime6=""
-        stime7=""
-        stime8=""
-        stime9=""
-        stime10=""
-        stime11=""
-        stime12=""
-        stime13=""
-        stime14=""
-        stime15=""
-        stime16=""
-        stime17=""
-        stime18=""
-        stime19=""
-        stime20=""
-        stime21=""
-        stime22=""
+        stime = {}
         query_job = client.query(query)
         for row in query_job:
             if row['Booking']==False:
                 if row['hour']==1:
-                    stime1=row['Start_Time']
-                    
+                    stime.add(row['Start Time'])                    
                 if row['hour']==2:
-                    stime2=row['Start_Time']
+                    stime.add(row['Start Time']) 
 
                 if row['hour']==3:
-                    stime3=row['Start_Time']
+                    stime.add(row['Start Time']) 
 
                 if row['hour']==4:
-                    stime4=row['Start_Time']
+                    stime.add(row['Start Time']) 
 
                 if row['hour']==5:
-                    stime5=row['Start_Time']
+                    stime.add(row['Start Time']) 
 
                 if row['hour']==6:
-                    stime6=row['Start_Time']
+                    stime.add(row['Start Time']) 
 
                 if row['hour']==7:
-                    stime7=row['Start_Time']
+                    stime.add(row['Start Time']) 
 
                 if row['hour']==8:
-                    stime8=row['Start_Time']
+                    stime.add(row['Start Time']) 
 
                 if row['hour']==9:
-                    stime9=row['Start_Time']
+                    stime.add(row['Start Time']) 
 
                 if row['hour']==10:
-                    stime10=row['Start_Time']
+                    stime.add(row['Start Time']) 
 
                 if row['hour']==11:
-                    stime11=row['Start_Time']
+                    stime.add(row['Start Time']) 
 
                 if row['hour']==12:
-                    stime12=row['Start_Time']
+                    stime.add(row['Start Time']) 
         
                 if row['hour']==13:
-                    stime13=row['Start_Time']
+                    stime.add(row['Start Time']) 
 
                 if row['hour']==14:
-                    stime14=row['Start_Time']
+                    stime.add(row['Start Time']) 
 
                 if row['hour']==15:
-                    stime15=row['Start_Time']            
+                    stime.add(row['Start Time'])             
             
                 if row['hour']==16:
-                    stime16=row['Start_Time']
+                    stime.add(row['Start Time']) 
 
                 if row['hour']==17:
-                    stime17=row['Start_Time']
+                    stime.add(row['Start Time']) 
                     
                 if row['hour']==18:
-                    stime18=row['Start_Time']
+                    stime.add(row['Start Time']) 
                     
                 if row['hour']==19:
-                    stime19=row['Start_Time']
+                    stime.add(row['Start Time']) 
                     
                 if row['hour']==20:
-                    stime20=row['Start_Time']
+                    stime.add(row['Start Time']) 
                 
                 if row['hour']==21:
-                    stime21=row['Start_Time']
+                    stime.add(row['Start Time']) 
 
                 if row['hour']==22:
-                    stime22=row['Start_Time']
+                    stime.add(row['Start Time']) 
             else:
                 pass
                               
@@ -592,10 +570,7 @@ def IndexResident():
             else:
                 pass
 
-        return render_template("indexResident.html",username=username,name=name, blockNum=blockNum,unitNum=unitNum,stime1=stime1,
-        stime2=stime2,stime3=stime3,stime4=stime4,stime5=stime5,stime6=stime6,stime7=stime7,stime8=stime8,stime9=stime9,stime10=stime10,
-        stime11=stime11,stime12=stime12,stime13=stime13,stime14=stime14,stime15=stime15,stime16=stime16,stime17=stime17,stime18=stime18,
-        stime19=stime19,stime20=stime20,stime21=stime21,stime22=stime22,c2stime1=c2stime1,c2stime2=c2stime2,c2stime3=c2stime3,
+        return render_template("indexResident.html",username=username,name=name, blockNum=blockNum,unitNum=unitNum,stime=jsonify(stime),c2stime1=c2stime1,c2stime2=c2stime2,c2stime3=c2stime3,
         c2stime4=c2stime4,c2stime5=c2stime5,c2stime6=c2stime6,c2stime7=c2stime7,c2stime8=c2stime8,c2stime9=c2stime9,c2stime10=c2stime10,
         c2stime11=c2stime11,c2stime12=c2stime12,c2stime13=c2stime13,c2stime14=c2stime14,c2stime15=c2stime15,c2stime16=c2stime16,
         c2stime17=c2stime17,c2stime18=c2stime18,c2stime19=c2stime19,c2stime20=c2stime20,c2stime21=c2stime21,c2stime22=c2stime22,
