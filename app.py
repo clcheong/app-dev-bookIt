c538998427a60373911dd41d8009080d5c5c48e1
Brandon <brandon_1331@hotmail.com>
2021-11-28 11:14:15 +0800

    Jsonify

--- app.py
+++ app.py
@@ -748,8 +748,8 @@ def jsontest():
             'Reservation Status':status, 
             'Reservation Time':stime,
             'Booking ID':book_ID}
-            jsonStr=json.dumps(reservationlist)
             reservationlist.append(reservation)
+    jsonStr=json.dumps(reservationlist)
     return jsonify(Reservations=jsonStr)
 
 
